fdf1a2e996c081a135c8623c7a0bc1cac660cefb
Taarun <taarunnp06@gmail.com>
2026-09-11 19:50:35 +0530

    Update dev_setup.sh

--- scripts/dev_setup.sh
+++ scripts/dev_setup.sh
@@ -5,7 +5,7 @@ PROJECT_ROOT=$(git rev-parse --show-toplevel)
 # Default to minimal installation unless --full flag passed
 INSTALL_FULL=false  
 DEVCONTAINER=false     
-for arg in "$@"  
+for arg in "$@"   
 do   
     if [ "$arg" = "--full" ] 
     then    
